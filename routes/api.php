8c301e1d6d2df7f149dee7b18d6f6ab35ab560ae
KraAlv <santito.sfg@gmail.com>
2025-03-28 22:52:56 -0400

    feat: API´s configuration "area´s registration"

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+//areas
+Route::get('/areas', function(){
+    return "     de areas";
+});
