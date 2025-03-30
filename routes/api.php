40304b9e42d4098e8bfd955d92cbd3ff367c00d3
AndyOrtz <andy.ortiz.f21@gmail.com>
2025-03-30 02:34:24 -0400

    fix: fixed 'register' in route to 'store' and added get route

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\OlympiadRegistrationController;
+use App\Http\Controllers\OlympiadRegistrationController;
 
-Route::post('/olympiad-registration', [OlympiadRegistrationController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/olympiad-registration', [OlympiadRegistrationController::class, 'store']);
+Route::get('/olympiad-registration', [OlympiadRegistrationController::class, 'index']);
