bf0b29a79f1347bbbb36e96b7096c3abc4bb8874
AndyOrtz <andy.ortiz.f21@gmail.com>
2025-03-30 01:55:08 -0400

    refactor(model): update fillable fields for OlympiadRegistration

--- app/Models/OlympiadRegistration.php
+++ app/Models/OlympiadRegistration.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class OlympiadRegistration extends Model
 {
     //
+    protected $table = 'olimpiadas';
+    public $timestamps = false;
+    
+    protected $primaryKey = 'id_olimpiada';
+    protected $fillable = [
+        'gestion',
+        'costo',
+        'fecha_inicio',
+        'fecha_fin',
+        'creado_en',
+        'max_categorias_olimpista'
+    ];
+    public function areas()
+    {
+        return $this->hasMany(Area::class, 'id_olimpiada');
+    }
+    //
 }
