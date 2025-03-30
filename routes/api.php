9032a74a3c87fd23ef2d39e49a20e28fa3e21c7e
AndyOrtz <andy.ortiz.f21@gmail.com>
2025-03-30 00:12:16 -0400

    fix(api): add missing slash to route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\OlympiadRegistrationController;
 
-Route::post('olympiad-registration', [OlympiadRegistrationController::class, 'register']);
+Route::post('/olympiad-registration', [OlympiadRegistrationController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
