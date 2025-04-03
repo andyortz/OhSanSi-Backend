bfec13ac658a46601eb11fddd795224f38690d66
KraAlv <santito.sfg@gmail.com>
2025-04-03 18:13:27 -0400

    fix: incripciones' table fixed

--- database/migrations/2025_03_29_073112_create_inscripciones_table.php
+++ database/migrations/2025_03_29_073112_create_inscripciones_table.php
@@ -14,18 +14,17 @@ return new class extends Migration
         Schema::create('inscripciones', function (Blueprint $table) {
             $table->id('id_inscripcion');
 
-            $table->unsignedBigInteger('id_olimpiada');
             $table->unsignedBigInteger('id_olimpista');
-            $table->unsignedBigInteger('id_nivel');
             $table->unsignedBigInteger('id_pago');
+            $table->unsignedBigInteger('id_categoria');
+            
 
             $table->timestamp('fecha_inscripcion', 6)->useCurrent();
             $table->string('estado', 50)->default('pendiente');
 
             // Foreign keys
-            $table->foreign('id_olimpiada')->references('id_olimpiada')->on('olimpiadas')->onDelete('cascade');
             $table->foreign('id_olimpista')->references('id_olimpista')->on('olimpistas')->onDelete('cascade');
-            $table->foreign('id_nivel')->references('id_nivel')->on('niveles_categoria')->onDelete('cascade');
+            $table->foreign('id_categoría')->references('id_niveles')->on('niveles_categoria')->onDelete('cascade');
             $table->foreign('id_pago')->references('id_pago')->on('pagos')->onDelete('cascade');
         });
     }
