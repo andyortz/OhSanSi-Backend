a955122915dac6e4b284dbc26f9353a2caccb939
KraAlv <santito.sfg@gmail.com>
2025-04-03 20:55:01 -0400

    feat: AreasSeeder update

---
+++ database/seeders/AreasSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AreasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $areas =[
+            ['id_area'=>1, 'nombre'=>'MATEMÁTICAS'],
+            ['id_area'=>2, 'nombre'=>'BIOLOGÍA'],
+            ['id_area'=>3, 'nombre'=>'ASTRONOMÍA - ASTROFÍSICA'],
+            ['id_area'=>4, 'nombre'=>'ROBÓTICA'],
+            ['id_area'=>5, 'nombre'=>'INFORMÁTICA'],
+            ['id_area'=>6, 'nombre'=>'QUÍMICA'],
+            // ['nombre'=>'Geografia'],
+            // ['nombre'=>'Historia'],
+        ];
+
+        DB::table('areas_competencia')->insert($areas);
+    }
+}
