66a9c1febbb08216daba5e13fa7f198a4767d5f3
KraAlv <santito.sfg@gmail.com>
2025-04-14 10:34:27 -0400

    fix: model of parentesco, tutores database

--- database/migrations/2025_03_29_073029_create_tutores_table.php
+++ database/migrations/2025_03_29_073029_create_tutores_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('ci');
             $table->integer('celular');
             $table->string('correo_electronico', 100);
-            $table->enum('rol_parentesco', ['Tutor Legal', 'Tutor Academico']);
 
         });
     }
