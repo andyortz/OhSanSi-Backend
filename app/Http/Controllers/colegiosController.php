43e80eddd39b3153952746a09a3e907990373baf
KraAlv <santito.sfg@gmail.com>
2025-04-14 13:33:48 -0400

    feat: get colegios by provincia

--- app/Http/Controllers/colegiosController.php
+++ app/Http/Controllers/colegiosController.php
@@ -14,4 +14,15 @@ class colegiosController extends Controller
         $colegios = Colegio::all();
         return response()->json($colegios, 200);
     }
+    public function porProvincia($id)
+    {
+        $colegios = Colegio::where('provincia', $id)->get();
+        if($colegios->isEmpty()){
+            return response()->kson([
+                'message' =>'No se encontraron colegios para esta provincia.',
+                'status' => 404 
+            ]);
+        }
+        return response()->json($colegios, 200);
+    }
 }
