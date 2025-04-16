d5b84bba35336d580d1d95bfe44b2af1c388df97
AndyOrtz <andy.ortiz.f21@gmail.com>
2025-04-16 05:00:11 -0400

    feat: endpoint to multiple olimpistas and one tutor academico

--- routes/api.php
+++ routes/api.php
@@ -53,6 +53,10 @@ Route::post('/inscripciones', [InscripcionNivelesController::class, 'store']);
 // inscrpcion con posible tutor
 Route::post('/inscripciones-con-tutor', [InscripcionNivelesController::class, 'storeWithTutor']);
 
+// inscripcion de varios olimpistas con un tutor
+Route::post('/registrar-varios-olimpistas', [InscripcionNivelesController::class, 'registrarVarios']);
+
+
 // inscripciones por olimpista
 Route::get('/olimpista/{ci}/inscripciones', [VerificarInscripcionController::class, 'getInscripcionesPorCI']);
 Route::get('/olimpista/{ci}/total-inscripciones', [VerificarInscripcionController::class, 'getTotalInscripciones']);
