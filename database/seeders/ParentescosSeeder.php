760285eb8b51e3eba12d541a624008a2744da7ac
KraAlv <santito.sfg@gmail.com>
2025-04-16 06:44:32 -0400

    feat: Seeders Olimpista, Tutores, parentescos Updated

--- database/seeders/ParentescosSeeder.php
+++ database/seeders/ParentescosSeeder.php
@@ -4,14 +4,32 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Parentesco;
+use Illuminate\Support\Facades\DB;
 
 class ParentescosSeeder extends Seeder
 {
     public function run(): void
     {
-        Parentesco::create(
-            ['id_olimpista' => 1,'id_tutor' => 1,'rol_parentesco' => 'Tutor Academico'],
-            []
-        );
+        
+        $parentescos= [
+            ['id_olimpista' => 1, 'id_tutor' => 1, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 2, 'id_tutor' => 2, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 3, 'id_tutor' => 3, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 4, 'id_tutor' => 4, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 5, 'id_tutor' => 5, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 6, 'id_tutor' => 6, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 7, 'id_tutor' => 7, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 8, 'id_tutor' => 8, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 9, 'id_tutor' => 9, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 10, 'id_tutor' => 1, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 11, 'id_tutor' => 2, 'rol_parentesco' => 'Tutor Legal'],
+            ['id_olimpista' => 1, 'id_tutor' => 3, 'rol_parentesco' => 'Tutor Academico'],
+            ['id_olimpista' => 3, 'id_tutor' => 5, 'rol_parentesco' => 'Tutor Academico'],
+            ['id_olimpista' => 5, 'id_tutor' => 7, 'rol_parentesco' => 'Tutor Academico'],
+            ['id_olimpista' => 7, 'id_tutor' => 9, 'rol_parentesco' => 'Tutor Academico'],
+            ['id_olimpista' => 9, 'id_tutor' => 2, 'rol_parentesco' => 'Tutor Academico'],
+            ['id_olimpista' => 11, 'id_tutor' => 4, 'rol_parentesco' => 'Tutor Academico']
+        ];
+        DB::table('parentescos')->insert($parentescos);
     }
 }
--- database/seeders/TutoresSeeder.php
+++ database/seeders/TutoresSeeder.php
@@ -11,13 +11,15 @@ class TutoresSeeder extends Seeder
     public function run(): void
     {
         $tutores = [
-            ['nombres' => 'Leti', 'apellidos' => 'Blanco', 'ci' => 9, 'celular' => 9, 'correo_electronico' => 'leti@gmail.com'],
             ['nombres' => 'Rudolf', 'apellidos' => 'Mamani', 'ci' => 3718953, 'celular' => 16815249, 'correo_electronico' => 'Rudolfcito@gmail.com'],
             ['nombres' => 'Albert', 'apellidos' => 'Quispe', 'ci' => 785392561, 'celular' => 79246923, 'correo_electronico' => 'albert@gmail.com'],
             ['nombres' => 'Diego', 'apellidos' => 'Lizard', 'ci' => 4672351, 'celular' => 78235612, 'correo_electronico' => 'Diego23@gmail,com'],
             ['nombres' => 'Lady', 'apellidos' => 'Jackson Sanchez', 'ci' => 93274325, 'celular' => 46232854, 'correo_electronico' => 'gaga@gmail.com'],
             ['nombres' => 'Farieed', 'apellidos' => 'Chino Llusco', 'ci' => 9467821, 'celular' => 7894729, 'correo_electronico' => 'chinito@gmail.com'],
             ['nombres' => 'Judas', 'apellidos' => 'Abracadabra', 'ci' => 3321159, 'celular' => 54323719, 'correo_electronico' => 'judas@gmail.com'],
+            ['nombres' => 'Bruno', 'apellidos' => 'Mars Quispe', 'ci' => 12321159, 'celular' => 54211319, 'correo_electronico' => 'ElMarciano@gmail.com'],
+            ['nombres' => 'Evo', 'apellidos' => 'Morales', 'ci' => 58492015, 'celular' => 68323719, 'correo_electronico' => 'papaEvo@gmail.com'],
+            ['nombres' => 'Tuto', 'apellidos' => 'Linera', 'ci' => 47286959, 'celular' => 73052167, 'correo_electronico' => 'tutitopro@gmail.com'],
         ];
         DB::table('tutores')->insert($tutores);
     }
