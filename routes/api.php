fd0cd9b66487908c4bc14e9d8608080c61a11cab
KraAlv <santito.sfg@gmail.com>
2025-05-01 23:12:36 -0400

    feat: implement storeOne method for individual inscription handling and update API routes

--- routes/api.php
+++ routes/api.php
@@ -58,6 +58,7 @@ Route::get('/olimpiadas/{id}/max-categorias', [OlimpiadaAreaController::class, '
 
 //Route::post('/inscripciones', [InscripcionAreaController::class, 'store']);
 Route::post('/inscripciones', [InscripcionNivelesController::class, 'store']);
+Route::post('/inscripcionesOne', [InscripcionNivelesController::class, 'storeOne']);
 
 // inscrpcion con posible tutor
 Route::post('/inscripciones-con-tutor', [InscripcionNivelesController::class, 'store']);
