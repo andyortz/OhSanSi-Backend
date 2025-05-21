9928221ac052c75d4b46bd940cbe392003ee0462
Bruno Salcedo <bruno.salcedo.ca@gmail.com>
2025-05-20 23:59:44 -0400

    fix: correct query condition in ColegioResolver to use 'id_provincia'

--- app/Services/ImportHelpers/ColegioResolver.php
+++ app/Services/ImportHelpers/ColegioResolver.php
@@ -8,7 +8,7 @@ class ColegioResolver
 {
     public static function resolve(int $idDepartamento, int $idProvincia): int
     {
-        $colegio = Colegio::where('provincia', $idProvincia)->first();
+        $colegio = Colegio::where('id_provincia', $idProvincia)->first();
 
         // Si no se encuentra, devolver el id "Otro"
         return $colegio ? $colegio->id_colegio : 1;  // 1 es el ID de "Otros"
